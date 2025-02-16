f278687a6c426d956c84d4981648044dc00dc2d9
Den Amiel Leonardo <denamielleonardo@192.168.1.17>
2025-02-16 14:38:58 +0800

    added animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  texts = [
+    "Welcome to my world!",
+    "Hi, I'm <span>Den Amiel</span>\na Software Developer",
+    "Passionate about building innovative and scalable solutions, turning ideas into reality with clean and efficient code."
+  ];
+  displayedText: string[] = ["", "", ""];
+  currentIndex = 0;
+  isTypingComplete: boolean = false;
+
   constructor() { }
+
+  ngAfterViewInit(): void {
+    this.startTyping(0, 0); // Start from the first text and first character
+  }
+
+  startTyping(index: number, charIndex: number) {
+    if (index >= this.texts.length) {
+      this.isTypingComplete = true;
+      return;
+    }
+  
+    let originalText = this.texts[index];
+    let result = "";
+    let insideTag = false;
+    let tagBuffer = "";
+  
+    const interval = setInterval(() => {
+      if (charIndex < originalText.length) {
+        const char = originalText[charIndex];
+  
+        if (char === "<") {
+          insideTag = true;
+          tagBuffer = char;
+        } else if (char === ">") {
+          insideTag = false;
+          tagBuffer += char;
+          result += tagBuffer;
+        } else if (insideTag) {
+          tagBuffer += char;
+        } else {
+          result += char;
+        }
+  
+        this.displayedText[index] = result;
+        charIndex++;
+      } else {
+        clearInterval(interval);
+        setTimeout(() => this.startTyping(index + 1, 0), 20); // Faster transition
+      }
+    }, 30); // Faster typing speed
+  }
 }
